e100eedc9fbdd3d43117e847805a6f08cc79a652
a <80372683+Amabellzq@users.noreply.github.com>
2022-12-17 03:04:05 +0800

    Added lib files

---
+++ lib/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+    {
+      id: 'Chicken Rice',
+      name: 'Plate',
+      description: 'A delicious box of handmade cookies',
+      unitName: 'box', // shows after the price, eg. 0.05 SOL/box
+      priceSol: 0.05,
+      priceUsd: 5,
+    },
+    {
+      id: 'basket-of-cookies',
+      name: 'Basket',
+      description: 'A large basket of handmade cookies',
+      unitName: 'basket',
+      priceSol: 0.1,
+      priceUsd: 10,
+    }
+  ]
